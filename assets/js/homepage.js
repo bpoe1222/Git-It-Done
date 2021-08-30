343bbbe7cb2422d21e607a966bc1768255138212
Bailey Poe <baileypoe1222@gmail.com>
2021-08-30 17:07:42 -0500

    added functionality so users can click on specific repos and see their listed issues

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -3,6 +3,7 @@ var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
 
+
 var getUserRepos = function(user) {
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
     fetch(apiUrl).then(function (response) {
@@ -42,14 +43,21 @@ var displayRepos = function (repos, searchTerm) {
     repoSearchTerm.textContent = searchTerm;
 
     for (var i = 0; i < repos.length; i++) {
+
         var repoName = repos[i].owner.login + "/" + repos[i].name;
-        var repoEl = document.createElement("div");
+
+        var repoEl = document.createElement("a");
         repoEl.classList = "list-item flex-row justify-space-between align-center";
+        repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
+
         var titleEl = document.createElement("span");
         titleEl.textContent = repoName;
+
         repoEl.appendChild(titleEl);
+
         var statusEl = document.createElement("span");
-        statusEl.classList = "flex-row align-center"
+        statusEl.classList = "flex-row align-center";
+
         if (repos[i].open_issues_count > 0) {
             statusEl.innerHTML = 
             "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
